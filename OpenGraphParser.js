8716dfde860bf21ffecb5eafcc6ed94d23e23c2e
Adrien Thiery <adrien.thiery@osedea.com>
2017-01-04 16:39:15 -0500

    We now return the meta data of the first url that has og-data

--- OpenGraphParser.js
+++ OpenGraphParser.js
@@ -1,9 +1,9 @@
 import decodeHTMLChars from './decodeHTMLChars';
 
-function parseMeta(html) {
+function parseMeta(html, url) {
     const metaTagRegex = /<meta.*property=[ '"]*og:.*/gi;
     const meta = {
-        url: '',
+        url: url,
     };
     const matches = html.match(metaTagRegex);
 
@@ -33,6 +33,13 @@ function parseMeta(html) {
 
             if (metaValue.length > 1) {
                 metaValue = metaValue[1].split(metaValue[1].trim()[0])[1];
+                if (metaValue[0] === '/') {
+                    if (url[url.length - 1] === '/') {
+                        metaValue = url + metaValue.substring(1);
+                    } else {
+                        metaValue = url + metaValue;
+                    }
+                }
             } else {
                 break;
             }
@@ -44,11 +51,12 @@ function parseMeta(html) {
     return meta;
 }
 
-async function fetchHtml(url) {
+async function fetchHtml(urlToFetch) {
     let result;
 
     try {
-        result = await fetch(url);
+        result = await fetch(urlToFetch);
+
         if (result.status >= 400) {
             throw result;
         }
@@ -57,41 +65,60 @@ async function fetchHtml(url) {
         .then((resultParsed) => (resultParsed));
     } catch (responseOrError) {
         if (responseOrError.message) {
-            throw responseOrError;
+            console.log(responseOrError);
+            return null;
         }
 
         return responseOrError.text()
         .then((error) => {
-            throw new Error('An error has occured while fetching url content', error);
+            console.log('An error has occured while fetching url content', error);
+            return null;
         });
     }
 }
 
-function getFirstUrl(content) {
+function getUrls(contentToMatch) {
     const regexp = /((?:(http|https|Http|Https|rtsp|Rtsp)?:?\/?\/?(?:(?:[a-zA-Z0-9\$\-_\.\+!\*'\(\),;\?&=]|(?:%[a-fA-F0-9]{2})){1,64}(?::(?:[a-zA-Z0-9\$\-_\.\+!\*'\(\),;\?&=]|(?:%[a-fA-F0-9]{2})){1,25})?@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?::\d{1,5})?)(\/(?:(?:[a-zA-Z0-9;\/\?:@&=#~\-\.\+!\*'\(\),_])|(?:%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;
-    const url = regexp.exec(content);
+    const urls = contentToMatch.match(regexp);
+    const urlsToReturn = [];
 
-    if (url) {
-        if (url[0].toLowerCase().indexOf('http') === 0) {
-            return url[0];
-        } else {
-            return `http://${url[0]}`;
-        }
+    if (urls && urls.length) {
+        urls.forEach((url) => {
+            if (url.toLowerCase().indexOf('http') === 0) {
+                urlsToReturn.push(url);
+            } else {
+                urlsToReturn.push(`http://${url}`);
+            }
+        });
+
+        return urlsToReturn;
     } else {
         throw new Error('Could not find an html link');
     }
 }
 
-async function extractMeta(content = '') {
+async function extractMeta(textContent = '') {
     try {
-        const url = getFirstUrl(content);
+        const urls = getUrls(textContent);
 
-        return await fetchHtml(url).then(
-            (html) => ({
-                ...parseMeta(html),
-                url,
-            })
-        );
+        console.log('Extracted urls', urls);
+
+        let metaData = null;
+        let i = 0;
+
+        while (!metaData && i < urls.length) {
+            metaData = await fetchHtml(urls[i])
+                .then(
+                    (html) => ({
+                        ...parseMeta(html, urls[i]),
+                        url: urls[i],
+                    })
+                );
+
+            i++;
+        }
+
+        return metaData;
     } catch (e) {
         console.log(e);
 
